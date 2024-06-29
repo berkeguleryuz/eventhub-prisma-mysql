0e18e4ffc5a33da224a15c6898f907a86befc0a6
Berke Güleryüz <berkeguleryuz@gmail.com>
2024-06-29 12:40:47 +0200

    update

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,3 +1,4 @@
+import { db } from "@/lib/db";
 import { stripe } from "@/lib/stripe";
 import { headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
@@ -19,5 +20,26 @@ export const POST = async (req: NextRequest) => {
     return new NextResponse(`Webhook error: ${err.message}`, { status: 400 });
   }
 
-  const session = event.data.object as Stripe.Checkout.Session
+  const session = event.data.object as Stripe.Checkout.Session;
+  const customerId = session?.metadata?.customerId;
+  const courseId = session?.metadata?.courseId;
+
+  if (event.type === "checkout.session.completed") {
+    if (!customerId || !courseId) {
+      return new NextResponse("Missing metadata", { status: 400 });
+    }
+
+    await db.purchase.create({
+      data: {
+        customerId,
+        courseId,
+      },
+    });
+  } else {
+    return new NextResponse(`Unhandled event type ${event.type}`, {
+      status: 400,
+    });
+  }
+
+  return new NextResponse("Success", { status: 200 });
 };
