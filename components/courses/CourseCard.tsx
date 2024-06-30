b72afe347a3ee292b0f18bc3d281cbd6df1761d3
Berke Güleryüz <berkeguleryuz@gmail.com>
2024-06-30 13:19:41 +0200

    update

--- components/courses/CourseCard.tsx
+++ components/courses/CourseCard.tsx
@@ -59,7 +59,7 @@ const CourseCard = async ({ course }: { course: Course }) => {
             </div>
           )}
         </div>
-        <p className="text-sm font-bold">$ {course.price}</p>
+        <p className="text-sm font-bold">€ {course.price}</p>
       </div>
     </Link>
   );
